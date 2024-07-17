a087704da9520088694b5a0e5c5253b38aae135d
sobirov-abdujalil <abdujalilsobirovfronted@gmail.com>
2024-07-16 22:18:26 -0700

    update

--- 4.Variables/main.py
+++ 4.Variables/main.py
@@ -163,3 +163,41 @@ Global Variables
 """
 
 #Create a variable outside of a function, and use it inside the function
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+"""
+If you create a variable with the same name inside a function, this variable will be local, 
+and can only be used inside the function. The global variable with the same name will remain
+as it was, global and with the original value.
+"""
+
+#Create a variable inside a function, with the same name as the global variable
+x = "awesome"
+
+def myfunc():
+	x = "fantastic"
+	print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+"""
+The global Keyword
+
+Normally, when you create a variable inside a function, that variable is local, and can only be used inside that function.
+To create a global variable inside a function, you can use the global keyword.
+"""
+
+#If you use the global keyword, the variable belongs to the global scope
+def myfunc():
+	global x
+	x = "fantastic"
+myfunc()
+
+print("Python is " + x)
