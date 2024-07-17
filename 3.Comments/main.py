0bf1d2d9d526d1640c23027c8de41c5b39923fa5
sobirov-abdujalil <abdujalilsobirovfronted@gmail.com>
2024-07-16 19:45:46 -0700

    update

---
+++ 3.Comments/main.py
@@ -0,0 +1,19 @@
+#This is a comment
+print("Hello, World!")
+"""
+This comment
+written in
+more than just one line
+"""
+#This is one line comment
+
+""" 
+Why we using comment?
+
+1.Comments can be used to explain Python code.
+
+2.Comments can be used to make the code more readable.
+
+3.Comments can be used to prevent execution when testing code.
+
+"""
