7326724a25bf582d864063797e2dcd5d64ecd2aa
sobirov-abdujalil <abdujalilsobirovfronted@gmail.com>
2024-07-16 22:23:31 -0700

    update

--- 4.Variables/main.py
+++ 4.Variables/main.py
@@ -201,3 +201,16 @@ def myfunc():
 myfunc()
 
 print("Python is " + x)
+
+#Also, use the global keyword if you want to change a global variable inside a function.
+#To change the value of a global variable inside a function, refer to the variable by using
+#the global keyword:
+x = "awesome"
+
+def myfunc():
+	global x 
+	x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
