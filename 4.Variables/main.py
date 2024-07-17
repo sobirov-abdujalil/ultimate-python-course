2b8818660caa6dd2af748920777f76397aa86f13
sobirov-abdujalil <abdujalilsobirovfronted@gmail.com>
2024-07-16 20:41:25 -0700

    update

--- 4.Variables/main.py
+++ 4.Variables/main.py
@@ -95,3 +95,28 @@ myVariableName = "John"
 MyVariableName = "John"
 #Snake Case
 my_variable_name = "John"
+
+#Python Variables - Assign Multiple Values
+
+#Many Values to Multiple Variables
+x, y, z = "Orange", "Banana","Cherry"
+print(x)
+print(y)
+print(z)
+
+#One Value to Multiple Variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#Unpack a Collection
+
+#If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking.
+
+#Unpack a list:
+fruits = ["apple","banana","cherry"]
+x,y,z = fruits
+print(x)
+print(y)
+print(z)
